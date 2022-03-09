b006239cb1c53d1dbbc22f6b233b824864afde2a
krcafferty01 <98491243+krcafferty01@users.noreply.github.com>
2022-03-08 20:55:33 -0500

    Update AdminController.java
    hardcoded database connection to patients table.

--- ProjectRIS/src/Controllers/AdminController.java
+++ ProjectRIS/src/Controllers/AdminController.java
@@ -49,4 +49,44 @@ public class AdminController {
 		Main m = new Main();
 		m.changeScene("../Views/Admin.fxml");
 	}
+	public void addNewPatient(ActionEvent event) throws IOException {
+		String firstName = "'Kevin'";
+		String lastName = "'Cafferty'";
+		String dob = "'2001-05-14'";
+		String sex = "'m'";
+		String race = "'white'";
+		String ethnicity = "'white'";
+		
+		 Connection conn = null;
+	      Statement stmt = null;
+	      try {
+	          try {
+	             Class.forName("com.mysql.jdbc.Driver");
+	          } catch (Exception e) {
+	             System.out.println(e);
+	       }
+	       conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ris", "root", "ENTER YOUR PASSWORD HERE");
+	       System.out.println("Connection is created successfully:");
+	       stmt = (Statement) conn.createStatement();
+	       String query1 = "INSERT INTO patients " + "VALUES (1, " + firstName + ", " + lastName + ", " + dob + ", " + sex + ", " + race + ", " + ethnicity + ")";;
+	       stmt.executeUpdate(query1);
+	       System.out.println("Record is inserted in the table successfully..................");
+	       } catch (SQLException excep) {
+	          excep.printStackTrace();
+	       } catch (Exception excep) {
+	          excep.printStackTrace();
+	       } finally {
+	          try {
+	             if (stmt != null)
+	                conn.close();
+	          } catch (SQLException se) {}
+	          try {
+	             if (conn != null)
+	                conn.close();
+	          } catch (SQLException se) {
+	             se.printStackTrace();
+	          }  
+	       }
+	       System.out.println("Please check it in the MySQL Table......... ……..");
+	    }
 }
