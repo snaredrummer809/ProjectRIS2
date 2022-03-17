e64337cb5ce7e9472331189c5378adf7cc9fffd1
SamuelStubb <98777575+SamuelStubb@users.noreply.github.com>
2022-03-16 20:05:14 -0400

    Update DatabaseConnection.java

--- ProjectRIS/src/application/DatabaseConnection.java
+++ ProjectRIS/src/application/DatabaseConnection.java
@@ -8,8 +8,8 @@ public class DatabaseConnection {
 	
 	public static Connection getConnection() throws SQLException {
 		
-		String username = "root";
-	    String password = "Sjs063099!";
+	    String username = "root";
+	    String password = "Enter your password";
 	    String url = "jdbc:mysql://localhost:3306/db_ris";
 
 		Connection con = DriverManager.getConnection(url, username, password);
