8aa9ba4e9debf3a5cc5b0588ff630c885e0465e6
SamuelStubb <98777575+SamuelStubb@users.noreply.github.com>
2022-03-26 16:47:00 -0400

    Update ModelTable.java

--- ProjectRIS/src/Controllers/ModelTable.java
+++ ProjectRIS/src/Controllers/ModelTable.java
@@ -1,64 +1,93 @@
 package Controllers;
 
 public class ModelTable {
-	String username,displayName,email,role;
-	int ID;
+	String s1,s2,s3,s4,s5,s6;
+	int num1, num2, num3;
 
-	public ModelTable(int ID, String username, String displayName, String email, String role) {
+	public ModelTable(int num1, int num2, int num3, String s1, String s2, String s3, String s4, String s5, String s6) {
 		super();
-		this.ID = ID;
-		this.username = username;
-		this.displayName = displayName;
-		this.email = email;
-		this.role = role;
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
+		this.s1 = s1;
+		this.s2 = s2;
+		this.s3 = s3;
+		this.s4 = s4;
+		this.s5 = s5;
+		this.s6 = s6;
 	}
 
-	public ModelTable(String username, String displayName, String email, String role, int iD) {
-		super();
-		this.username = username;
-		this.displayName = displayName;
-		this.email = email;
-		this.role = role;
-		this.ID = iD;
-	}
-	public int getID() {
-		return ID;
+	public String getS1() {
+		return s1;
 	}
 
-	public void setID(int ID) {
-		this.ID = ID;
+	public void setS1(String s1) {
+		this.s1 = s1;
 	}
 
-	public String getUsername() {
-		return username;
+	public String getS2() {
+		return s2;
 	}
 
-	public void setUsername(String username) {
-		this.username = username;
+	public void setS2(String s2) {
+		this.s2 = s2;
 	}
 
-	public String getDisplayName() {
-		return displayName;
+	public String getS3() {
+		return s3;
 	}
 
-	public void setDisplayName(String displayName) {
-		this.displayName = displayName;
+	public void setS3(String s3) {
+		this.s3 = s3;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getS4() {
+		return s4;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setS4(String s4) {
+		this.s4 = s4;
 	}
 
-	public String getRole() {
-		return role;
+	public String getS5() {
+		return s5;
 	}
 
-	public void setRole(String role) {
-		this.role = role;
+	public void setS5(String s5) {
+		this.s5 = s5;
+	}
+
+	public String getS6() {
+		return s6;
+	}
+
+	public void setS6(String s6) {
+		this.s6 = s6;
+	}
+
+	public int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+	public int getNum2() {
+		return num2;
+	}
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+
+	public int getNum3() {
+		return num3;
+	}
+
+	public void setNum3(int num3) {
+		this.num3 = num3;
 	}
 	
 }
+
