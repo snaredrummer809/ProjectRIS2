811c8886e99f21ebc780de7521105dd09e4b904e
krcafferty01 <98491243+krcafferty01@users.noreply.github.com>
2022-03-29 16:08:22 -0400

    Update AdminController.java
    addNewOrder() Method

--- ProjectRIS/src/Controllers/AdminController.java
+++ ProjectRIS/src/Controllers/AdminController.java
@@ -35,7 +35,16 @@ public class AdminController {
 	private DatePicker DOB;
 	@FXML 
 	ChoiceBox<String> sexChoiceBox;
-
+	
+	//orders
+	@FXML
+	private TextField OrderPatientName;
+	@FXML
+	private TextField OrderReferralMD;
+	@FXML
+	private TextField OrderModalityNeeded;
+	@FXML
+	private TextArea OrderNotes;
 	
 	public void userLogOut(ActionEvent event) throws IOException {
 		
@@ -136,4 +145,43 @@ public class AdminController {
 	       System.out.println("Please check it in the MySQL Table......... ……..");
 	    }
 	 
+	public void addNewOrder(ActionEvent event) throws IOException {
+		 Connection conn = null;
+	      Statement stmt = null;
+	      
+	      
+	      try {
+	          try {
+	             Class.forName("com.mysql.cj.jdbc.Driver");
+	          } catch (Exception e) {
+	             System.out.println(e);
+	       }
+	       conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/db_ris", "root", "Brikev34$");
+	       System.out.println("Connection is created successfully:");
+	       stmt = (Statement) conn.createStatement();
+	       
+	       
+	       String query1 = "INSERT INTO orders(patient, referral_md, modality, notes) VALUES('"+OrderPatientName.getText()+"', '"+OrderReferralMD.getText()+"', '"+OrderModalityNeeded.getText()+"', '"+OrderNotes.getText()+"');";
+	       System.out.println(query1);
+	       stmt.executeUpdate(query1);
+	       System.out.println("Record is inserted in the table successfully..................");
+	       } catch (SQLException excep) {
+	          excep.printStackTrace();
+	       } catch (Exception excep) {
+	          excep.printStackTrace();
+	       } finally {
+	          try {
+	             if (stmt != null)
+	                conn.close();
+	          } catch (SQLException se) {}
+	          try {
+	             if (conn != null)
+	                conn.close();
+	          } catch (SQLException se) {
+	             se.printStackTrace();
+	          }  
+	       }
+	       System.out.println("Please check it in the MySQL Table......... ……..");
+	       OrderButton(event);
+	      }
 	}
