31fe07db8674d4ce648e7c1e75fbe6596af93fbd
wileyjones18 <98791675+wileyjones18@users.noreply.github.com>
2022-03-29 20:26:29 -0400

    adding import statement
    fixing error; TextArea import

--- ProjectRIS/src/Controllers/AdminController.java
+++ ProjectRIS/src/Controllers/AdminController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextArea;
 
 public class AdminController {
 	
