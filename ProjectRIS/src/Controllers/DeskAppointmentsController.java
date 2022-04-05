85499a29ce434f2b4579dd10f2c0c49b3f0c7969
SamuelStubb <98777575+SamuelStubb@users.noreply.github.com>
2022-04-04 20:30:43 -0400

    Add files via upload

---
+++ ProjectRIS/src/Controllers/DeskAppointmentsController.java
@@ -0,0 +1,218 @@
+package Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+import application.DatabaseConnection;
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.util.Callback;
+
+public class DeskAppointmentsController implements Initializable{
+		
+		// Nav buttons
+		@FXML
+		Button HomeButton;
+		@FXML
+		Button ordersButton;
+		@FXML
+		Button LogOut;
+		
+		// Appointment Pane
+		@FXML
+		TableView<ModelTable> deskAppsTable;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsPatientColumn;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsModalityColumn;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsDateAndTimeColumn;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsTechNameColumn;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsRadiologistColumn;
+		@FXML
+		TableColumn<ModelTable, String> deskAppsDeleteColumn;
+		ObservableList<ModelTable> appointments = FXCollections.observableArrayList();
+		
+		//appDeleteConfirmationPane
+		@FXML
+		Pane appDeleteConfirmationPane;
+		@FXML
+		TextField appIDTextField;
+		@FXML
+		Button appConfirmDeleteButton;
+		
+		//Alerts
+		Alert updateAlert = new Alert(AlertType.CONFIRMATION);
+		
+		public void userLogOut(ActionEvent event) throws IOException {
+
+			Main m = new Main();
+
+			m.changeScene("../Views/Login.fxml");
+		}
+
+		public void HomeButton(ActionEvent event) throws IOException {
+
+			Main m = new Main();
+			m.changeScene("../Views/Desk.fxml");
+		}
+
+		public void OrderButton(ActionEvent event) throws IOException {
+
+			Main m = new Main();
+			m.changeScene("../Views/DeskOrders.fxml");
+		}
+		
+		public void initialize(URL arg0, ResourceBundle arg1) {
+			
+			populateAppointments();
+			updateAlert.setHeaderText("Success");
+
+		}
+		
+		public void populateAppointments() {
+			appointments.clear();
+			int patient = 0;
+			int modality = 0;
+			int tech = 0;
+			int radio = 0;
+			String patientName = null;
+			String modalityName = null;
+			String techName = null;
+			String radioName = null;
+			
+			
+			try {
+				Connection con = DatabaseConnection.getConnection();
+				ResultSet rs = con.createStatement().executeQuery("select * from appointments");
+
+				while (rs.next()) {
+					patient = rs.getInt("patient");
+					modality = rs.getInt("modality");
+					tech = rs.getInt("technician");
+					radio = rs.getInt("radiologist");
+					ResultSet rs2 = con.createStatement().executeQuery("select * from patients where patient_id=" + patient);
+					while(rs2.next()) {
+						patientName = rs2.getString("first_name") + " " + rs2.getString("last_name");
+					}
+					rs2 = con.createStatement().executeQuery("select * from modalities where modality_id=" + modality);
+					while(rs2.next()) {
+						modalityName = rs2.getString("name");
+					}
+					rs2 = con.createStatement().executeQuery("select * from users where user_id=" + tech);
+					while(rs2.next()) {
+						techName = rs2.getString("full_name");
+					}
+					rs2 = con.createStatement().executeQuery("select * from users where user_id=" + radio);
+					while(rs2.next()) {
+						radioName = rs2.getString("full_name");
+					}				
+					
+					appointments.add(new ModelTable(patient, 0, 0, patientName,
+							modalityName, rs.getString("date_time"), techName, 
+							radioName, null));
+				}
+			} 
+			catch (SQLException e) {
+				System.out.println("Error: Could not get appointment data.");
+			}
+			
+			deskAppsPatientColumn.setCellValueFactory(new PropertyValueFactory<>("s1"));
+			deskAppsModalityColumn.setCellValueFactory(new PropertyValueFactory<>("s2"));
+			deskAppsDateAndTimeColumn.setCellValueFactory(new PropertyValueFactory<>("s3"));
+			deskAppsTechNameColumn.setCellValueFactory(new PropertyValueFactory<>("s4"));
+			deskAppsRadiologistColumn.setCellValueFactory(new PropertyValueFactory<>("s5"));
+			Callback<TableColumn<ModelTable, String>, TableCell<ModelTable, String>> cellFactory = (param) -> {
+
+				final TableCell<ModelTable, String> cell = new TableCell<ModelTable, String>() {
+
+					@Override
+					public void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+
+						if (empty) {
+							setText(null);
+						} else {
+							final Button modButton = new Button("Delete");
+							modButton.setOnAction(event -> {
+								ModelTable m = getTableView().getItems().get(getIndex());
+
+								appDeleteConfirmationPane.setVisible(true);
+								HomeButton.setDisable(true);
+								ordersButton.setDisable(true);
+								LogOut.setDisable(true);
+
+								appIDTextField.setText(m.getNum1()+"");
+
+							});
+
+							setGraphic(modButton);
+							setText(null);
+						}
+					}
+				};
+
+				return cell;
+			};
+			
+			// deskAppsDeleteColumn.setCellFactory(cellFactory);
+
+			deskAppsTable.setItems(appointments);
+		}
+		
+		public void appConfirmDelete(ActionEvent event) throws IOException {
+			
+			try {
+				Connection con = DatabaseConnection.getConnection();
+				Statement stmt = con.createStatement();
+				
+				String deleteApp = "delete from appointments where appointment_id=" + appIDTextField.getText();
+				stmt.executeUpdate(deleteApp);
+				
+				String updateOrders = "update orders set appointment= 0 where appointment=" 
+						+ appIDTextField.getText();
+				stmt.executeUpdate(updateOrders);
+				
+				con.close();
+				
+				appDeleteConfirmationPane.setVisible(false);
+				HomeButton.setDisable(false);
+				ordersButton.setDisable(false);
+				LogOut.setDisable(false);
+				updateAlert.setContentText("Appointment has been successfully deleted.");
+				updateAlert.showAndWait();
+				populateAppointments();
+			}
+			catch(SQLException e) {
+				System.out.println("Error: Could not delete appointment.");
+			}
+		}
+		
+		public void cancelButton(ActionEvent event) throws IOException {
+			HomeButton.setDisable(false);
+			ordersButton.setDisable(false);
+			LogOut.setDisable(false);
+			appDeleteConfirmationPane.setVisible(false);
+			
+		}
+}
---
+++ ProjectRIS/src/Controllers/DeskOrdersController.java
@@ -0,0 +1,229 @@
+package Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+import application.DatabaseConnection;
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.util.Callback;
+
+public class DeskOrdersController implements Initializable{
+		
+	// Nav buttons
+	@FXML
+	Button HomeButton;
+	@FXML
+	Button AppointmentButton;
+	@FXML
+	Button LogOut;
+	
+	// Appointment Pane
+	@FXML
+	TableView<ModelTable> deskAllOrdersTable;
+	@FXML
+	TableColumn<ModelTable, Integer> deskAllOrdersOrderIDCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersPatientCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersModalityCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersReferralDocCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersNotesCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersStatusCol;
+	@FXML
+	TableColumn<ModelTable, String> deskAllOrdersDeleteCol;
+	ObservableList<ModelTable> orders = FXCollections.observableArrayList();
+	
+	//appDeleteConfirmationPane
+	@FXML
+	Pane allOrdersDeleteConfirmationPane;
+	@FXML
+	TextField allOrdersIDTextField;
+	@FXML
+	Button allOrdersConfirmDeleteButton;
+	
+	//Alerts
+	Alert updateAlert = new Alert(AlertType.CONFIRMATION);
+	
+	public void userLogOut(ActionEvent event) throws IOException {
+
+		Main m = new Main();
+
+		m.changeScene("../Views/Login.fxml");
+	}
+
+	public void HomeButton(ActionEvent event) throws IOException {
+
+		Main m = new Main();
+		m.changeScene("../Views/Desk.fxml");
+	}
+
+	public void AppointmentButton(ActionEvent event) throws IOException {
+		Main m = new Main();
+		m.changeScene("../Views/DeskAppointments.fxml");
+	}
+	
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		populateOrders();
+
+	}
+	
+	public void populateOrders() {
+		orders.clear();
+		int orderID = 0;
+		int patient = 0;
+		int doc = 0;
+		int modality = 0;
+		int status = 0;
+		String patientName = null;
+		String docName = null;
+		String modalityName = null;
+		String notes = null;
+		String statusName = null;
+		
+		
+		try {
+			Connection con = DatabaseConnection.getConnection();
+			ResultSet rs = con.createStatement().executeQuery("select * from orders");
+
+			while (rs.next()) {
+				orderID = rs.getInt("order_id");
+				System.out.println(orderID);
+				patient = rs.getInt("patient");
+				System.out.println(patient);
+				doc = rs.getInt("referral_md");
+				System.out.println(doc);
+				notes = rs.getString("notes");
+				System.out.println(notes);
+				modality = rs.getInt("modality");
+				System.out.println(modality);
+				status = rs.getInt("status");
+				System.out.println(status);
+				ResultSet rs2 = con.createStatement().executeQuery("select * from patients where patient_id=" + patient);
+				while(rs2.next()) {
+					patientName = rs2.getString("first_name") + " " + rs2.getString("last_name");
+				}
+				System.out.println(patientName);
+				rs2 = con.createStatement().executeQuery("select * from modalities where modality_id=" + modality);
+				while(rs2.next()) {
+					modalityName = rs2.getString("name");
+				}
+				System.out.println(modalityName);
+				rs2 = con.createStatement().executeQuery("select * from users where user_id=" + doc);
+				while(rs2.next()) {
+					docName = rs2.getString("full_name");
+				}
+				System.out.println(docName);
+				rs2 = con.createStatement().executeQuery("select * from order_status where order_status_id=" + status);
+				while(rs2.next()) {
+					statusName = rs2.getString("name");
+				}
+				orders.add(new ModelTable(orderID, 0, 0, patientName, docName
+						, modalityName, notes, 
+						statusName, null));
+			}
+		} 
+		catch (SQLException e) {
+			System.out.println("Error: Could not get order data.");
+		}
+		
+		deskAllOrdersOrderIDCol.setCellValueFactory(new PropertyValueFactory<>("num1"));
+		deskAllOrdersPatientCol.setCellValueFactory(new PropertyValueFactory<>("s1"));
+		deskAllOrdersReferralDocCol.setCellValueFactory(new PropertyValueFactory<>("s2"));
+		deskAllOrdersModalityCol.setCellValueFactory(new PropertyValueFactory<>("s3"));
+		deskAllOrdersNotesCol.setCellValueFactory(new PropertyValueFactory<>("s4"));
+		deskAllOrdersStatusCol.setCellValueFactory(new PropertyValueFactory<>("s5"));
+		Callback<TableColumn<ModelTable, String>, TableCell<ModelTable, String>> cellFactory = (param) -> {
+
+			final TableCell<ModelTable, String> cell = new TableCell<ModelTable, String>() {
+
+				@Override
+				public void updateItem(String item, boolean empty) {
+					super.updateItem(item, empty);
+
+					if (empty) {
+						setText(null);
+					} else {
+						final Button modButton = new Button("Delete");
+						modButton.setOnAction(event -> {
+							ModelTable m = getTableView().getItems().get(getIndex());
+
+							allOrdersDeleteConfirmationPane.setVisible(true);
+							HomeButton.setDisable(true);
+							AppointmentButton.setDisable(true);
+							LogOut.setDisable(true);
+							
+							System.out.println(m.getNum1());
+							allOrdersIDTextField.setText(m.getNum1()+"");
+
+						});
+
+						setGraphic(modButton);
+						setText(null);
+					}
+				}
+			};
+
+			return cell;
+		};
+		// allOrdersDeleteCol.setCellFactory(cellFactory); 
+
+		deskAllOrdersTable.setItems(orders);
+	}
+	
+	public void allOrdersConfirmDelete(ActionEvent event) throws IOException {
+		
+		try {
+			Connection con = DatabaseConnection.getConnection();
+			Statement stmt = con.createStatement();
+			
+			String deleteApp = "delete from orders where appointment_id=" + allOrdersIDTextField.getText();
+			stmt.executeUpdate(deleteApp);
+			
+			con.close();
+			
+			allOrdersDeleteConfirmationPane.setVisible(false);
+			HomeButton.setDisable(false);
+			AppointmentButton.setDisable(false);
+			LogOut.setDisable(false);
+			updateAlert.setHeaderText("Success");
+			updateAlert.setContentText("Order has been successfully deleted.");
+			updateAlert.showAndWait();
+			populateOrders();
+		}
+		catch(SQLException e) {
+			System.out.println("Error: Could not delete order.");
+		}
+	}
+	
+	public void cancelButton(ActionEvent event) throws IOException {
+		HomeButton.setDisable(false);
+		AppointmentButton.setDisable(false);
+		LogOut.setDisable(false);
+		allOrdersDeleteConfirmationPane.setVisible(false);
+		
+	}
+
+}
