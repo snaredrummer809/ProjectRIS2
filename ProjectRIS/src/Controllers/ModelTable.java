c772a69fb931943fea0ca278f50249a498d56474
krcafferty01 <98491243+krcafferty01@users.noreply.github.com>
2022-04-07 00:52:02 -0400

    Update ModelTable.java

--- ProjectRIS/src/Controllers/ModelTable.java
+++ ProjectRIS/src/Controllers/ModelTable.java
@@ -22,6 +22,21 @@ public class ModelTable {
 		this.s5 = s5;
 		this.s6 = s6;
 	}
+	
+	public ModelTable(String s1, String s2, String s3, String s4, String s5, String s6, String s7, String s8, String s9, String s10) {
+		super();
+		this.s1 = s1;
+		this.s2 = s2;
+		this.s3 = s3;
+		this.s4 = s4;
+		this.s5 = s5;
+		this.s6 = s6;
+		this.s7 = s7;
+		this.s8 = s8;
+		this.s9 = s9;
+		this.s10 = s10;
+		
+	}
 
 	public String getS1() {
 		return s1;
@@ -71,6 +86,38 @@ public class ModelTable {
 		this.s6 = s6;
 	}
 
+	public String getS7() {
+		return s7;
+	}
+
+	public void setS7(String s7) {
+		this.s7 = s7;
+	}
+	
+	public String getS8() {
+		return s8;
+	}
+
+	public void setS8(String s8) {
+		this.s8 = s8;
+	}
+	
+	public String getS9() {
+		return s9;
+	}
+
+	public void setS9(String s9) {
+		this.s9 = s9;
+	}
+	
+	public String getS10() {
+		return s10;
+	}
+
+	public void setS10(String s10) {
+		this.s10 = s10;
+	}
+	
 	public int getNum1() {
 		return num1;
 	}
