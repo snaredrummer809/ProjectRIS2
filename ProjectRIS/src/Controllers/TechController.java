a06ceae2cc01046637d035a196ec276526214412
wileyjones18 <98791675+wileyjones18@users.noreply.github.com>
2022-04-06 21:28:39 -0400

    Update TechController.java
    added controls to upload images

--- ProjectRIS/src/Controllers/TechController.java
+++ ProjectRIS/src/Controllers/TechController.java
@@ -1,9 +1,13 @@
 package Controllers;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,7 +29,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.util.Callback;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class TechController implements Initializable{
 	
@@ -62,7 +70,14 @@ public class TechController implements Initializable{
 	TextField imagingModalityTextField;
 	@FXML 
 	TextArea imagingMDNotesTextArea;
-
+	@FXML Button chooseFileButton;
+	private FileChooser fileChooser;
+	private File file;
+	private Stage stage;
+	private final Desktop deskTop = Desktop.getDesktop();
+	@FXML private ImageView scanImageView;
+	private Image image;
+	private FileInputStream fis;
 
 	// Patient Overview pane
 	@FXML Pane patientOverviewPane;
@@ -76,9 +91,20 @@ public class TechController implements Initializable{
 	@FXML TextField overviewEmailTextField;
 	@FXML TextArea overviewAllergiesTextArea;
 	@FXML Button closeButton;
+	ModelTable instance = new ModelTable();
+	ModelTable m;
+	
+
+	
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		populateCheckedInApps();
+		
+		fileChooser = new FileChooser();
+		fileChooser.getExtensionFilters().addAll(
+					new FileChooser.ExtensionFilter("All files", "*.*"),
+					new FileChooser.ExtensionFilter("Images", "*.png", "*.jpg")
+		);
 	}
 	
 	// Populate checkedInAppsTable
@@ -96,6 +122,7 @@ public class TechController implements Initializable{
 		String techName = null;
 		String radioName = null;
 		String statusName = null;
+		int closed = 0;
 		
 		// Get all appointment data
 		try {
@@ -109,9 +136,12 @@ public class TechController implements Initializable{
 				radio = rs.getInt("radiologist");
 				order = rs.getInt("order_id");
 				checkedIn = rs.getInt("checked_in");
+				closed = rs.getInt("closed");
+				
 				
 				// Check to make sure appointment has been checked in
-				if(checkedIn == 1) {
+				//added check to make sure appt is not closed
+				if(checkedIn == 1  && closed==0) {
 					ResultSet rs2 = con.createStatement().executeQuery("select * from patients where patient_id=" + patient);
 					while(rs2.next()) {
 						patientName = rs2.getString("first_name") + " " + rs2.getString("last_name");
@@ -168,7 +198,7 @@ public class TechController implements Initializable{
 					} else {
 						final Button compOrderButton = new Button("Complete Order");
 						compOrderButton.setOnAction(event -> {
-							ModelTable m = getTableView().getItems().get(getIndex());
+							m = getTableView().getItems().get(getIndex());
 							String firstName = null;
 							String lastName = null;
 							String DOB = null;
@@ -180,6 +210,7 @@ public class TechController implements Initializable{
 							String allergies = null;
 							String modalityName = null;
 							
+											
 							try {
 								Connection con = DatabaseConnection.getConnection();
 								ResultSet rs = con.createStatement().executeQuery("select * from patients where patient_id=" + m.getNum1());
@@ -196,7 +227,22 @@ public class TechController implements Initializable{
 									allergies = rs.getString("allergies");
 									
 								}
+								con.close();
+							}
+							catch(SQLException e) {
+								e.printStackTrace();
+							}
+							
+							try {
+								Connection con = DatabaseConnection.getConnection();
+								ResultSet rs = con.createStatement().executeQuery("select * from orders where status=1 and patient=" + m.getNum1());
 								
+								while(rs.next()) {
+									int order_id = rs.getInt("order_id");
+									instance.setNum4(order_id);
+									
+								}
+								con.close();
 							}
 							catch(SQLException e) {
 								e.printStackTrace();
@@ -209,10 +255,50 @@ public class TechController implements Initializable{
 								while(rs.next()) {
 									modalityName = rs.getString("name");
 								}
+								con.close();
 							}
 							catch(SQLException e) {
 								e.printStackTrace();
 							}
+							
+							//find appt_id where patient_id = m.getNum1() and checked_in=1 and closed = 0
+													
+							try {
+								Connection con = DatabaseConnection.getConnection();
+								ResultSet rs = con.createStatement().executeQuery("select * from appointments where patient=" + m.getNum1()+" and checked_in=1 and closed=0");
+								
+								while(rs.next()) {
+									int appt_id = rs.getInt("appointment_id");
+									int patient = rs.getInt("patient");
+									int modality = rs.getInt("modality");
+									int tech = rs.getInt("technician");
+																	
+									instance.setNum1(modality);
+									instance.setNum2(patient);
+									instance.setNum3(tech);
+								}
+								con.close();
+								
+							}
+							catch(SQLException e) {
+								e.printStackTrace();
+							}
+							
+							//get order number to get notes
+							try {
+								Connection con = DatabaseConnection.getConnection();
+								ResultSet rs = con.createStatement().executeQuery("select * from orders where order_id=" + instance.getNum4());
+								
+								while(rs.next()) {
+									String notes = rs.getString("notes");						
+									instance.setS1(notes);
+								}
+								con.close();
+							}
+							catch(SQLException e) {
+								e.printStackTrace();
+							}
+
 
 							overviewFirstNameTextField.setText(firstName);
 							overviewLastNameTextField.setText(lastName);
@@ -252,11 +338,74 @@ public class TechController implements Initializable{
 		ImagingOrderPane.setVisible(false);
 		patientOverviewPane.setVisible(false);
 		//add method to upload image!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!	
+		//modality = instance.getModality()
+		//patient = instance.getPatient()
+		//tech = instance.getTech()		
+		
+		//query info from imaging_info table to find last used imaging_id
+				String sql = "insert into imaging_info(imaging_id,image_url,imaging,modality,notes,patient,technician) VALUES (?,?,?,?,?,?,?)";
+				int imaging_id = 0;
+				String image_url = file.getAbsolutePath();
+				int modality = instance.getNum1();
+				String notes  = instance.getS1();
+				int patient =instance.getNum2();
+				int technician = instance.getNum3();
+							
+				try {
+					Connection con = DatabaseConnection.getConnection();
+					
+					//set imaging_id
+					Statement stmt = con.createStatement();
+					String IDCheck = "select * from imaging_info";
+					ResultSet rs = stmt.executeQuery(IDCheck);
+					
+					int c = 0;
+
+					while (rs.next()) {
+						c = rs.getInt("imaging_id");
+					}
+					imaging_id = c + 1;
+				
+					
+					
+					PreparedStatement pst = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);		
+					pst.setInt(1, imaging_id);
+					pst.setString(2, image_url);
+					fis = new FileInputStream(file);
+					pst.setBinaryStream(3,fis,file.length());
+					pst.setInt(4, modality);
+					pst.setString(5, notes);
+					pst.setInt(6, patient);
+					pst.setInt(7, technician);
+					
+					pst.executeUpdate();
+					//rs.pst.getGeneratedKeys();
+					//rs.next();
+					
+					
+					
+					System.out.println("image inserted successfully");
+					
+					//update order status to imaging complete
+					String updateOrderStatus = "UPDATE orders SET status=2,image="+imaging_id +" where order_id="+instance.getNum4()+";";
+					System.out.println(updateOrderStatus);
+					try{
+						stmt.executeUpdate(updateOrderStatus);
+					}
+					catch(SQLException e){
+						System.out.println("Order status update failed...");
+					}
+				}
+				catch(SQLException e) {
+					System.out.println("image failed to insert");
+					e.printStackTrace();
+				}
 	}
 	
 	//cancel order method
 	public void backButton(ActionEvent event) throws IOException {
 		ImagingOrderPane.setVisible(false);
+		
 	}
 
 
@@ -283,6 +432,26 @@ public class TechController implements Initializable{
 		overviewEmailTextField.setText("");
 		overviewAllergiesTextArea.setText("");
 
+	}
+	
+	
+	public void selectFile(ActionEvent event) {
+		stage = (Stage) ImagingOrderPane.getScene().getWindow();
+		file = fileChooser.showOpenDialog(stage);
+		
+		/*try {
+			deskTop.open(file);
+		} catch (IOException e) {
+			System.out.println("File failed to open.");
+			e.printStackTrace();
+		}
+		*/
+		if(file != null) {
+			System.out.println(""+file.getAbsolutePath());
+			image = new Image(file.getAbsoluteFile().toURI().toString(), scanImageView.getFitWidth(), scanImageView.getFitHeight(), true, true);
+			scanImageView.setImage(image);
+			scanImageView.setPreserveRatio(true);
+		}
 	}
 
 	public void userLogOut(ActionEvent event) throws IOException {
