68b81ce82c15b9a8a02ab2039917f1e59220ea9f
krcafferty01 <98491243+krcafferty01@users.noreply.github.com>
2022-04-16 14:57:25 -0400

    Revert "Update DoctorReferralController.java"
    This reverts commit ffc2fc21a4e258353051afe2b29a50a9cdf88471.

--- ProjectRIS/src/Controllers/DoctorReferralController.java
+++ ProjectRIS/src/Controllers/DoctorReferralController.java
@@ -27,8 +27,6 @@ import javafx.util.Callback;
 public class DoctorReferralController implements Initializable{
 	//Patients Pane	
 	@FXML
-	TextField searchReferralPatientTextField;
-	@FXML
 	TableView<PatientTableModel> patientsTable;
 	@FXML
 	TableColumn<PatientTableModel, Integer> patientIDCol; //these are by fx:id found in fxml
@@ -107,21 +105,5 @@ public class DoctorReferralController implements Initializable{
 		Main m = new Main();
 		m.changeScene("../Views/DoctorPatientOverview.fxml");
 		
-	}
-	
-	public void searchPatients() {
-		searchPatients.clear();
-		String userSearch = searchReferralPatientTextField.getText();
-		if(!userSearch.equals("")) {
-			for(int i = 0; i < patients.size(); i++) {
-				if(patients.get(i).getS1().contains(userSearch) || patients.get(i).getS2().contains(userSearch)){
-					searchPatients.add(patients.get(i));
-				}
-			}
-			patientsTable.setItems(searchPatients);
-		}
-		else {
-			populatePatients();
-		}
 	}
 }
